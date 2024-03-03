341a80f9b2fd4548bf6052d07208fb13bf70ac4e
Sirian <42908738+siriannn@users.noreply.github.com>
2024-03-03 14:39:41 +1100

    adjusting camera

--- hardware/camera.py
+++ hardware/camera.py
@@ -6,6 +6,8 @@ import requests
 def capture_and_send_image():
     # Access the webcam
     cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_BRIGHTNESS, 5) # only for sirian's computer because my webcam is fucked.
+    cap.set(cv2.CAP_PROP_SATURATION, 5)
 
     # Capture a frame
     ret, frame = cap.read()
@@ -14,10 +16,22 @@ def capture_and_send_image():
     _, img_encoded = cv2.imencode('.jpg', frame)
     image_data = base64.b64encode(img_encoded)
 
+    
+    # Release the webcam
+    cap.release()
+
     # Send image data to backend
-    url = 'http://ec2-13-236-116-118.ap-southeast-2.compute.amazonaws.com:5000/detect_image'  # Replace with the actual backend server IP address
+    url = 'http://ec2-3-106-231-109.ap-southeast-2.compute.amazonaws.com:5000/detect_image'  # Replace with the actual backend server IP address
+    # url = "http://localhost:5000/detect_image"
     payload = {'image': image_data.decode('utf-8')}
-    headers = {'Content-Type': 'application/json'}
+    headers = requests.utils.default_headers()
+    headers.update(
+        {
+            'Content-Type': 'application/json',
+            'User-Agent': 'My User Agent 1.0'
+
+        }
+    )
     response = requests.post(url, json=payload, headers=headers)
 
     # Check response status
@@ -26,8 +40,24 @@ def capture_and_send_image():
     else:
         print('Failed to upload image')
 
-    # Release the webcam
-    cap.release()
+
+def test():
+    url = 'http://ec2-13-236-116-118.ap-southeast-2.compute.amazonaws.com:5000/detect_image'
+    # url = "http://127.0.0.1:5000/detect_image"
+    files = { 'media': open('placeholder.png', 'rb')}
+    payload = {'image': image_data.decode('utf-8')}
+    headers = requests.utils.default_headers()
+    headers.update(
+        {
+            'Content-Type': 'application/json',
+            'User-Agent': 'My User Agent 1.0'
+
+        }
+    )
+    response = requests.post(url, json=payload, headers=headers)
+
+    requests.post(url, files=files, headers=headers)
 
 # Capture and send image when the script is executed
 capture_and_send_image()
+# test()
